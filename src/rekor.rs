651d0f2ce147dfd0105ede04f099f01602f76b7c
jyotsna-penumaka <jpenumak@redhat.com>
2022-02-11 15:11:04 -0500

    Make a mock Rekor server
    Signed-off-by: jyotsna-penumaka <jpenumak@redhat.com>

--- src/rekor.rs
+++ src/rekor.rs
@@ -45,7 +45,7 @@ pub struct Signature {
     pub public_key: PublicKey,
 }
 
-/// Stores the public key used to sign the artifact 
+/// Stores the public key used to sign the artifact
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PublicKey {
@@ -68,7 +68,7 @@ pub struct Hash {
     pub value: String,
 }
 
-/// Stores the response returned by Rekor after making a new entry 
+/// Stores the response returned by Rekor after making a new entry
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Post {
@@ -98,7 +98,6 @@ pub async fn rekor_upload(
     algorithm_name: String,
     hash_val: String,
 ) -> Result<Post, Box<dyn Error>> {
-
     // This is the body of the post request
     let body = Root {
         api_version: api_version_val,
@@ -121,23 +120,47 @@ pub async fn rekor_upload(
         },
     };
 
-    // make a post request to create an entry in Rekor
+    /*
+    (Not for testing)
+    To make a post request to create an entry in the live Rekor server :
+
     let response = reqwest::Client::new()
-        //.post("https://rekor.sigstore.dev/api/v1/log/entries")
-        .post("https://api.rekor.dev/api/v1/log/entries")
+        .post("https://rekor.sigstore.dev/api/v1/log/entries")
         .json(&body)
         .send()
         .await?
         .text()
         .await?;
-    println!();
-    println!("{:#?}", response);
-    println!();
+    */
+
+    // Let's create a mock Rekor Server for testing
+    let server = httpmock::MockServer::start();
+    server.mock(|when, then| {
+        when.method("POST")
+            .path("/api/v1/log/entries")
+            .json_body_obj(&body);
+        then.status(201)
+            .body(
+    "{\"9bf4f37447a48848c1f69b6463190cd6cef7728d6b86b7723f08aad0c54cb8d4\":{\"body\":\"eyJhcGlWZXJzaW9uIjoiMC4wLjEiLCJraW5kIjoicmVrb3JkIiwic3BlYyI6eyJkYXRhIjp7Imhhc2giOnsiYWxnb3JpdGhtIjoic2hhMjU2IiwidmFsdWUiOiI0NmJkMzE5ZDM1OTBkMmI0ZDdjN2EyN2M5OWQzMmY3ZWE2MGE4NTBlYzM4MDYzNTFlMDRkMTYxZDAxNGVjYzAxIn19LCJzaWduYXR1cmUiOnsiY29udGVudCI6IkxTMHRMUzFDUlVkSlRpQlRVMGdnVTBsSFRrRlVWVkpGTFMwdExTMEtWVEZPU1ZVd2JFaEJRVUZCUVZGQlFVRkVUVUZCUVVGTVl6Tk9iMHhYVm10TmFsVXhUVlJyUVVGQlFXY3ZkbVZUWXpSdmJIQkxkRTF2VDFJM2NuZG1PQXBXUjBoNmFHaG5NRVpKYjBSMFl6VlNNa3BzZEhwSFowRkJRVUZGV20xc2MxcFJRVUZCUVVGQlFVRkJSMk15YUdoT1ZFVjVRVUZCUVZWM1FVRkJRWFI2Q21NeVozUmFWMUY1VGxSVmVFOVJRVUZCUlVKalEydDBaME14V1dwcmIzZEtkSEJzZVhCRFNEUTJhbkV5UW1Sb05tUjZhblIwZVd0SFpWRjVLMG8xZUhvS2RIRlVlbXRqUXpCWVNHRkZZWEZPZFdNMWNURnpUbEZNWTJRNFNEUjRNM0ZLU2xSRFFsRnZUd290TFMwdExVVk9SQ0JUVTBnZ1UwbEhUa0ZVVlZKRkxTMHRMUzBLIiwiZm9ybWF0Ijoic3NoIiwicHVibGljS2V5Ijp7ImNvbnRlbnQiOiJjM05vTFdWa01qVTFNVGtnUVVGQlFVTXpUbnBoUXpGc1drUkpNVTVVUlRWQlFVRkJTVkEzTTJ0dVQwdEtZVk55VkV0RWEyVTJPRWd2UmxKb09EUlpXVTVDVTB0Qk4xaFBWV1JwV21KamVHOEsifX19fQ==\",\"integratedTime\":1643749079,\"logID\":\"c0d23d6ad406973f9559f3ba2d1ca01f84147d8ffc5b8445c224f98b9591801d\",\"logIndex\":1236200,\"verification\":{\"signedEntryTimestamp\":\"MEQCID3QHHXwGauKUfFvs0YCMKZ4e3BE1ZIhrJ5RHnoHGophAiA/R5cDc8JnSUq6F7U/8oTk8/mn7QCdDXI+NRT7qm78+Q==\"}}}\n"
+            );
+    });
+
+    let response = reqwest::Client::new()
+        .post(server.url("/api/v1/log/entries"))
+        .json(&body)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    // println!();
+    // println!("{:#?}", response);
+    // println!();
 
     /*
     We need to modify the response before we can read it into a Struct
-    
-    This is the response that is returned:
+
+    This is the response that is returned by Rekor:
     {
         "9bf4f37447a48848c1f69b6463190cd6cef7728d6b86b7723f08aad0c54cb8d4": {
             "body": "eyJhcGlWZXJzaW9uIjoiMC4wLjEiLCJraW5kIjoicmVrb3JkIiwic3BlYyI6eyJkYXRhIjp7Imhhc2giOnsiYWxnb3JpdGhtIjoic2hhMjU2IiwidmFsdWUiOiI0NmJkMzE5ZDM1OTBkMmI0ZDdjN2EyN2M5OWQzMmY3ZWE2MGE4NTBlYzM4MDYzNTFlMDRkMTYxZDAxNGVjYzAxIn19LCJzaWduYXR1cmUiOnsiY29udGVudCI6IkxTMHRMUzFDUlVkSlRpQlRVMGdnVTBsSFRrRlVWVkpGTFMwdExTMEtWVEZPU1ZVd2JFaEJRVUZCUVZGQlFVRkVUVUZCUVVGTVl6Tk9iMHhYVm10TmFsVXhUVlJyUVVGQlFXY3ZkbVZUWXpSdmJIQkxkRTF2VDFJM2NuZG1PQXBXUjBoNmFHaG5NRVpKYjBSMFl6VlNNa3BzZEhwSFowRkJRVUZGV20xc2MxcFJRVUZCUVVGQlFVRkJSMk15YUdoT1ZFVjVRVUZCUVZWM1FVRkJRWFI2Q21NeVozUmFWMUY1VGxSVmVFOVJRVUZCUlVKalEydDBaME14V1dwcmIzZEtkSEJzZVhCRFNEUTJhbkV5UW1Sb05tUjZhblIwZVd0SFpWRjVLMG8xZUhvS2RIRlVlbXRqUXpCWVNHRkZZWEZPZFdNMWNURnpUbEZNWTJRNFNEUjRNM0ZLU2xSRFFsRnZUd290TFMwdExVVk9SQ0JUVTBnZ1UwbEhUa0ZVVlZKRkxTMHRMUzBLIiwiZm9ybWF0Ijoic3NoIiwicHVibGljS2V5Ijp7ImNvbnRlbnQiOiJjM05vTFdWa01qVTFNVGtnUVVGQlFVTXpUbnBoUXpGc1drUkpNVTVVUlRWQlFVRkJTVkEzTTJ0dVQwdEtZVk55VkV0RWEyVTJPRWd2UmxKb09EUlpXVTVDVTB0Qk4xaFBWV1JwV21KamVHOEsifX19fQ==",
@@ -149,11 +172,12 @@ pub async fn rekor_upload(
             }
         }
     }
-    
+
     Since there is no member called "uuid" in the json body,
-    we cannot read it into the Post struct. 
-    So we add "{\"uuid\": " to the returned response and make it a valid json 
+    we cannot read it into the Post struct.
+    So we add "{\"uuid\": " to the returned response and make it a valid json
     */
+
     let uuid: &str = &response[1..67];
     let rest: &str = &response[69..response.len() - 2];
     let sum = "{\"uuid\": ".to_string() + &(uuid.to_owned()) + "," + rest;
@@ -163,12 +187,12 @@ pub async fn rekor_upload(
 
 #[cfg(test)]
 mod tests {
-    
+
     use crate::rekor::rekor_upload;
-    
+
     #[tokio::test]
     async fn verify_rekor_upload() -> Result<(), reqwest::Error> {
-        let _response = rekor_upload(
+        let response = rekor_upload(
         "0.0.1".to_string(),
         "rekord".to_string(),
         "ssh".to_string(),
@@ -177,6 +201,15 @@ mod tests {
         "https://raw.githubusercontent.com/jyotsna-penumaka/integrate-rekor/main/README.md".to_string(),
         "sha256".to_string(),
         "58f7c1bab6fc37b4679abf5971898d0b61cd29c9afe153bfcfafabb23c256883".to_string()).await;
+        assert!(response.is_ok());
         Ok(())
     }
 }
+
+/*
+TO DO:
+1. replace isach with request once you get it to work
+2. Check if you can move httpmock = "0.6", isahc = "1.6.0" into dev-dependencies
+
+
+*/
