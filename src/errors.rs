c50f3319ff712e278d6c822009bda8fb2bf0b805
Dmitry S <dsavints@gmail.com>
2023-05-18 22:02:46 +0200

    update pkcs1 from 0.4.0 to 0.7.5
    Signed-off-by: Dmitry S <dsavints@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -207,6 +207,9 @@ pub enum SigstoreError {
     RSAError(#[from] rsa::errors::Error),
 
     #[error(transparent)]
+    RSAPKCS1Error(#[from] rsa::pkcs1::Error),
+
+    #[error(transparent)]
     PKCS1Error(#[from] pkcs1::Error),
 
     #[error(transparent)]
