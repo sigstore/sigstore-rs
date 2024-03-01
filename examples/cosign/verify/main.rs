77ed1f9e38bcef5e33c5b4b8717ddba69665ede9
Tanner Gill <contacttannergill@gmail.com>
2024-03-01 13:18:11 -0800

    Renamed impacted types in /examples
    Signed-off-by: Tanner Gill <contacttannergill@gmail.com>

--- examples/cosign/verify/main.rs
+++ examples/cosign/verify/main.rs
@@ -22,7 +22,7 @@ use sigstore::cosign::{CosignCapabilities, SignatureLayer};
 use sigstore::crypto::SigningScheme;
 use sigstore::errors::SigstoreVerifyConstraintsError;
 use sigstore::registry::{ClientConfig, ClientProtocol, OciReference};
-use sigstore::sigstore::SigstoreRepository;
+use sigstore::trust::sigstore::SigstoreRepository;
 use std::boxed::Box;
 use std::convert::TryFrom;
 use std::time::Instant;
@@ -110,7 +110,7 @@ struct Cli {
 
 async fn run_app(
     cli: &Cli,
-    frd: &dyn sigstore::repo::Repository,
+    frd: &dyn sigstore::trust::TrustRoot,
 ) -> anyhow::Result<(Vec<SignatureLayer>, VerificationConstraintVec)> {
     // Note well: this a limitation deliberately introduced by this example.
     if cli.cert_email.is_some() && cli.cert_url.is_some() {
@@ -228,7 +228,7 @@ async fn run_app(
     Ok((trusted_layers, verification_constraints))
 }
 
-async fn fulcio_and_rekor_data(cli: &Cli) -> anyhow::Result<Box<dyn sigstore::repo::Repository>> {
+async fn fulcio_and_rekor_data(cli: &Cli) -> anyhow::Result<Box<dyn sigstore::trust::TrustRoot>> {
     if cli.use_sigstore_tuf_data {
         let repo: sigstore::errors::Result<SigstoreRepository> = spawn_blocking(|| {
             info!("Downloading data from Sigstore TUF repository");
