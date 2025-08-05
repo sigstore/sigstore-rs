4ff6c4c5681511889e4d6e03a24360f42f0263fe
Flavio Castelli <fcastelli@suse.com>
2025-08-05 08:59:33 +0200

    chore(deps): fix compilation after cached update
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- src/registry/oci_caching_client.rs
+++ src/registry/oci_caching_client.rs
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::time::Duration;
+
 use super::{ClientCapabilities, ClientCapabilitiesDeps};
 use crate::errors::{Result, SigstoreError};
 
